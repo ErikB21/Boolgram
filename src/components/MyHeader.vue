8ba345930fb9850d2600eb538ae64e134332a41b
ErikBorgogno <erikborgo.20@gmail.com>
2022-12-07 03:30:21 +0100

    add OffCanvas

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -7,40 +7,12 @@
                 </a>
             </div>
 
-            
+            <MyOffCanvas/>
 
             <div class="d-none d-md-flex">
                 <form class="d-flex" role="search" style="width:400px;">
                     <input class="form-control rounded text-center me-2 p-2" type="search" placeholder="Search" aria-label="Search">
                 </form>
-            </div>
-
-            <!-- <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offCanvasNavbar" aria-controls="offCanvasNavbar" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button> -->
-
-            <button class="btn btn-primary" @click.prevent="showOffcanvasMenu()" type="button" data-bs-toggle="offcanvas" data-bs-target="#rightOffcanvas" aria-controls="rightOffcanvas">
-                Right Offcanvas
-            </button>
-
-            <div class="offcanvas offcanvas-end" :class="showMenu ? 'show' : ''" :style="{ visibility: showMenu ? 'visible' : 'hidden' }" tabindex="-1" id="rightOffcanvas" aria-labelledby="rightOffcanvasLabel">
-                <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="rightOffcanvasLabel">Right Offcanvas</h5>
-                    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div class="offcanvas-body">
-                    <ul>
-                    <li>
-                        <a href="#">Home</a>
-                    </li>
-                    <li>
-                        <a href="#">Posts</a>
-                    </li>
-                    <li>
-                        <a href="#">Settings</a>
-                    </li>
-                    </ul>
-                </div>
             </div>
 
             <div class="d-none d-lg-flex">
@@ -63,18 +35,10 @@
 </template>
 
 <script>
+import MyOffCanvas from './MyOffCanvas.vue'
 export default {
+  components: { MyOffCanvas },
     name: 'MyHeader',
-    data(){
-        return {
-            showMenu: false
-        }
-    },
-    methods: {
-        showOffcanvasMenu(){
-            this.showMenu ? this.showMenu = false : this.showMenu = true;
-        }
-    }
 }
 </script>
 
---
+++ src/components/MyOffCanvas.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <button @click.prevent="showOffcanvasMenu()" class="btn" data-bs-toggle="offcanvas" data-bs-target="#rightOffcanvas" aria-controls="rightOffcanvas">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+
+        <div class="offcanvas offcanvas-end" :class="showMenu ? 'show' : ''" :style="{ visibility: showMenu ? 'visible' : 'hidden' }" style="height:500px" tabindex="-1" id="rightOffcanvas" aria-labelledby="rightOffcanvasLabel">
+            <div class="offcanvas-header">
+                <button class="btn btn-close text-reset" @click.prevent="showOffcanvasMenu()" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body d-flex flex-column justify-content-between">
+                <ul class="d-flex list-unstyled flex-column justify-content-center align-items-center">
+                    <li class="nav-item">
+                        <a class="navbar-brand p-0 m-0 eb_rounded" href="#">
+                            <img src="../assets/profile.jpg" alt="Profile">
+                        </a>
+                    </li>
+                    <li class="nav-item d-flex flex-column pt-3 justify-content-between align-items-center">
+                        <span class="fw-bold fs-5">ErikB21</span>
+                        <span class="text-secondary fs-5">Erik Borgogno</span>
+                    </li>
+                </ul>
+                <ul class="d-flex list-unstyled flex-column">
+                    <li class="py-3">
+                        <a class="d-flex align-items-center text-decoration-none" href=""><i class="fa-solid fa-user fa-2x pe-2"></i> Profilo</a>
+                    </li>
+                    <li class="py-3">
+                        <a class="d-flex align-items-center text-decoration-none" href=""><i class="fa-solid fa-users fa-2x pe-2"></i> Amici</a>
+                    </li>
+                    <li class="py-3">
+                        <a class="d-flex align-items-center text-decoration-none" href=""><i class="fa-solid fa-gear fa-2x pe-2"></i>Impostazioni</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'MyOffCanvas',
+    data(){
+        return {
+            showMenu: false
+        }
+    },
+    methods: {
+        showOffcanvasMenu(){
+            this.showMenu ? this.showMenu = false : this.showMenu = true;
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    .eb_rounded{
+        width: 85px;
+        height: 85px;
+        border-radius: 50%;
+        img{
+            width: 85px;
+            height: 85px;
+            border-radius: 50%;
+            object-fit: contain;
+        }
+    }
+</style>
