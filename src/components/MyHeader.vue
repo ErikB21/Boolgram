9c77439e79858ebeab04ad1e0a9402d6c10510b5
ErikBorgogno <erikborgo.20@gmail.com>
2022-12-07 17:25:21 +0100

    add svg logo and fix layout

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,13 +1,15 @@
 <template>
     <nav class="navbar navbar-expand-lg border-bottom py-3">
         <div class="container-fluid d-flex justify-content-between justify-content-md-evenly aling-items-center">
-            <div class="pe-2">
-                <a class="navbar-brand" href="#">
+            <div class="d-flex align-items-center">
+                <router-link :to="{name: 'home'}"  class="d-none d-md-flex navbar-brand border-4 border-end pe-3" href="#">
+                    <img src="logo.svg" alt="Boolgram" width="40">
+                </router-link>
+                <router-link :to="{name: 'home'}"  class="navbar-brand" href="#">
                     <img src="../assets/logo.png" alt="Boolgram" width="120">
-                </a>
+                </router-link>
             </div>
 
-            <MyOffCanvas/>
 
             <div class="d-none d-md-flex">
                 <form class="d-flex" role="search" style="width:400px;">
@@ -15,13 +17,14 @@
                 </form>
             </div>
 
+
             <div class="d-none d-lg-flex">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-evenly align-items-center">
                     <li class="nav-item pe-2">
                         <a class="nav-link active" aria-current="page" href="#"><i class="fa-regular fa-heart fa-2x"></i></a>
                     </li>
                     <li class="nav-item pe-2">
-                        <a class="nav-link text-dark" href="#"><i class="fa-solid fa-house fa-2x"></i></a>
+                        <router-link class="nav-link text-dark" :to="{name: 'home'}"><i class="fa-solid fa-house fa-2x"></i></router-link>
                     </li>
                     <li class="nav-item">
                         <a class="navbar-brand eb_rounded" href="#">
@@ -29,6 +32,9 @@
                         </a>
                     </li>
                 </ul>
+            </div>
+            <div class="d-flex d-lg-none">
+                <MyOffCanvas/>
             </div>
         </div>
     </nav>
